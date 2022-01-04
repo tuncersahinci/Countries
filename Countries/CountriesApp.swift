1a98d98e15454534e12cd9c6e899d730ab2bcf66
Tuncer <40545015+Denerod@users.noreply.github.com>
2022-01-04 18:45:22 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Tuncer Şahinci on 4.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
